cc07713b50754c9e855b48dd2028e5191e668870
AzamovMuhammad <azamov.dragon@gmail.com>
2025-05-18 11:10:47 +0500

    setup Backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/auth", authRoutes);
+// app.use("/admin", adminRoutes);
+// app.use("/owner", ownerRoutes);
+// app.use("/user", userRoutes);
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(4000, () => {
+  console.log(`server ${PORT}-portda ishga tushdi`);
+});
